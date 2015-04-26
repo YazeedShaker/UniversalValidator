05e4df92cf0560e394737c0960d8ef8035abc478
hassanhabib <hassanhabib@live.com>
2015-04-26 18:11:58 -0500

    Added PhonesValidationTests class.

---
+++ UniversalValidatorUT/PhonesValidationsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UniversalValidatorUT
+{
+    [TestClass]
+    class PhonesValidationsTests
+    {
+        [TestMethod]
+        public void ValidateIfInputNonPhoneFails()
+        {
+            string nonePhone = "2832ksjd2";
+            bool pass = UniversalValidator.Phones.IsPhone(nonePhone);
+
+            Assert.IsFalse(pass);
+        }
+    }
+}
