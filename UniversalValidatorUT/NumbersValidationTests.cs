0b32c6354c7801d9cab33e0fea941cf5cccea636
hassanhabib <hassanhabib@live.com>
2015-04-26 16:02:54 -0500

    Added a unit test  project to the solution.

---
+++ UniversalValidatorUT/NumbersValidationTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UniversalValidatorUT
+{
+    [TestClass]
+    public class NumbersValidationTests
+    {
+        [TestMethod]
+        public void ValidateIfNumberIsNumberFail()
+        {
+            string input = "A";
+            bool pass = UniversalValidator.Numbers.IsNumeric(input);
+            Assert.IsFalse(pass);
+        }
+    }
+}
