3eb04fe6e7ee289f9bbc381a0981f6daf40f03d2
hassanhabib <hassanhabib@live.com>
2015-04-26 16:02:24 -0500

    Added a pilot method for checking whether an input is numeric or not.

---
+++ UniversalValidator/Numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalValidator
+{
+    public static class Numbers
+    {
+        public static bool IsNumeric(string input)
+        {
+            int outValue;
+            bool isNumeric = int.TryParse(input, out outValue);
+            return isNumeric;
+        }
+    }
+}
