28bd1b87c3dfe199655a72096b5448cd25b90201
hassanhabib <hassanhabib@live.com>
2015-04-26 18:12:29 -0500

    Added inital phone validation method.

--- UniversalValidator/Phones.cs
+++ UniversalValidator/Phones.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UniversalValidator
 {
-    class Phones
+    public static class Phones
     {
+        public static bool IsPhone(string input)
+        {
+            bool isPhone = false;
+            try
+            {
+                isPhone = Regex.IsMatch(input, @"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return isPhone;
+        }
     }
 }
