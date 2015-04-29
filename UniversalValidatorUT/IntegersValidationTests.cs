a9a1a9643012089db11874517a2988704c388af8
YazeedShaker <yazeed.shaker@outlook.com>
2015-04-29 17:21:24 +0200

    IntegersValidationTests

---
+++ UniversalValidatorUT/IntegersValidationTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UniversalValidatorUT
+{
+    [TestClass]
+    public class IntegersValidationTests
+    {
+
+        [TestMethod]
+        public void ValidateIfInputIsIntegerSucceed()
+        {
+            string input = "2,147,483,647";
+            bool pass = UniversalValidator.validateInt.isInt(input);
+            Assert.IsTrue(pass);
+        }
+
+    }
+}
