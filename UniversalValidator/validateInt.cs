d25b88f120c6f9fade39d1d775830dfead768667
YazeedShaker <yazeed.shaker@outlook.com>
2015-04-29 17:16:38 +0200

    validateIntegers

---
+++ UniversalValidator/validateInt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace UniversalValidator
+{
+    public static class validateInt
+    {
+        public static bool isInt(string input)
+        {
+            int i;
+            bool b = int.TryParse(input, NumberStyles.AllowThousands,CultureInfo.InvariantCulture,out i);
+            if (b)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
