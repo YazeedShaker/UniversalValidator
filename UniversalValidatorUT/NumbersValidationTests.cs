33ec2fb58d06835509ec8e0c255f5deb3aca8c6e
YazeedShaker <yazeed.shaker@outlook.com>
2015-04-29 17:55:47 +0200

    Numbers Validation tests

--- UniversalValidatorUT/NumbersValidationTests.cs
+++ UniversalValidatorUT/NumbersValidationTests.cs
@@ -17,8 +17,24 @@ namespace UniversalValidatorUT
         [TestMethod]
         public void ValidateIfInputIsNumericSucceed()
         {
-            string input = "-1.27982E+15";
+            string input = "1.23E+11";
             bool pass = UniversalValidator.Numbers.IsNumeric(input);
+            Assert.IsTrue(pass);
+        }
+
+        [TestMethod]
+        public void ValidateIfInputIsIntegerSucceed()
+        {
+            string input = "2,147,483,647";
+            bool pass = UniversalValidator.Integers.isInt(input);
+            Assert.IsTrue(pass);
+        }
+
+        [TestMethod]
+        public void ValidateIfInputIsFloatSucceed()
+        {
+            string input = "1.063E-02";
+            bool pass = UniversalValidator.Floats.isFloat(input);
             Assert.IsTrue(pass);
         }
     }
