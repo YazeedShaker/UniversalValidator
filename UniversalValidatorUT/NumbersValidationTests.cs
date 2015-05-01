db5b976f22a81bc9289dc1d3c8ce7b5714e235d7
hassanhabib <hassanhabib@live.com>
2015-04-30 22:01:45 -0500

    Merging

--- UniversalValidatorUT/NumbersValidationTests.cs
+++ UniversalValidatorUT/NumbersValidationTests.cs
@@ -15,9 +15,25 @@ namespace UniversalValidatorUT
         }
 
         [TestMethod]
-        public void ValidateIfInputIsNumericSucceed()
+        public void ValidateIfInputIsNumericScientificSucceed()
         {
             string input = "1.23E+11";
+            bool pass = UniversalValidator.Numbers.IsNumeric(input);
+            Assert.IsTrue(pass);
+        }
+
+        [TestMethod]
+        public void ValidateIfInputIsNumericRawSucceed()
+        {
+            string input = "234234234";
+            bool pass = UniversalValidator.Numbers.IsNumeric(input);
+            Assert.IsTrue(pass);
+        }
+
+        [TestMethod]
+        public void ValidateIfInputIsNumericCommaSucceed()
+        {
+            string input = "112,345,3456";
             bool pass = UniversalValidator.Numbers.IsNumeric(input);
             Assert.IsTrue(pass);
         }
